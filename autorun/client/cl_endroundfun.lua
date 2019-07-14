57911dff299439b70ff43b098dcb3302c70d566e
Sam <firesthatconsume@gmail.com>
2019-07-14 13:25:41 -0500

    added voting

--- autorun/client/cl_endroundfun.lua
+++ autorun/client/cl_endroundfun.lua
@@ -1,23 +1,125 @@
 net.Receive("firesttt_csaycustomround", function()
 	local name = net.ReadString()
 	local description = net.ReadString()
-	ULib.csayDraw("CUSTOM ROUND - "..name, Color(255, 0, 0, 255), 10, 0.7 )
-	chat.AddText(Color(255,0,0), "[CUSTOM ROUND] ", Color(255,255,255), name.." - "..description)
+	local modifier = net.ReadString()
+	if modifier == "none_included" then
+		ULib.csayDraw("CUSTOM ROUND - "..name, Color(255, 0, 0, 255), 10, 0.7 )
+		chat.AddText(Color(255,0,0), "[CUSTOM ROUND] ", Color(255,255,255), name.." - "..description)
+	else	
+		ULib.csayDraw("CUSTOM ROUND - "..name.."\nModifier - "..modifier, Color(255, 0, 0, 255), 10, 0.7 )
+		chat.AddText(Color(255,0,0), "[CUSTOM ROUND] ", Color(255,255,255), name.." - "..description)
+		chat.AddText(Color(255,0,0), "[CUSTOM ROUND] ", Color(255,255,255), "Modifier -"..modifier)
+	end
 end)
 
-function Draw()
+function DrawEndround()
    if not GetGlobalBool("EndRoundGameIsActive") then return end
    surface.SetFont("TabLarge")
    surface.SetTextColor(255, 0, 0, 230)
    local text = "Round is currently a custom gamemode."
    local w, h = surface.GetTextSize(text)
-
    surface.SetTextPos(30, ScrH() - 200 - h)
    surface.DrawText(text)
 end
 
 hook.Add("HUDPaint", "hudpaintdrawathing", function()
    if hook.Call( "HUDShouldDraw", GAMEMODE, "tttnotdisguise" ) then
-       Draw(LocalPlayer())
+       DrawEndround(LocalPlayer())
    end
 end)
+
+function ShowVotingFrame(died)
+	died = died or false
+	local frame = vgui.Create( "DFrame" )
+	frame:SetSize( ScrW() * 400/1920, ScrH() * 150/1080 )
+	frame:SetTitle( "Custom Rounds Voting" )
+	frame:Center()
+	frame:SetVisible( true )
+	frame:SetDraggable( false )
+	frame:MakePopup()
+	
+	local sheet = vgui.Create("DPropertySheet", frame)
+	sheet:Dock(FILL)
+	local onvote = true
+	function sheet:OnActiveTabChanged(old, new)
+		if onvote == true then
+			new:SizeTo(500,500, 1)
+			onvote = false
+		else
+			new:SizeTo(250,250, 1)
+			onvote = true 
+		end
+	end
+	
+	local panel1 = vgui.Create( "DPanel", sheet )
+	
+	local ques = vgui.Create("DLabel", panel1)
+	ques:SetText("Would you like a Custom Round before the map changes?")
+	ques:SetFont("DermaDefaultBold")
+	ques:SizeToContents()
+	ques:SetColor(Color(0,0,0))
+	ques:SetPos(ScrW() * 20 / 1920, ScrH() * 10 / 1080)
+	
+	local voteyes = vgui.Create( "DCheckBox", panel1 ) 
+	voteyes:SetPos(ScrW() *20/1920,ScrH() *30/1080)
+	local vylabel = vgui.Create("DLabel", panel1)
+	vylabel:SetText("Yes")
+	vylabel:SetColor(Color(0,0,0))
+	vylabel:SizeToContents()
+	vylabel:SetPos(ScrW() *40/1920, ScrH() *30/1080)
+	
+	local voteno = vgui.Create( "DCheckBox", panel1 ) 
+	voteno:SetPos(ScrW() *20/1920,ScrH() *50/1080)
+	local vnlabel = vgui.Create("DLabel", panel1)
+	vnlabel:SetText("No")
+	vnlabel:SetColor(Color(0,0,0))
+	vnlabel:SizeToContents()
+	vnlabel:SetPos(ScrW() * 40 / 1920, ScrH() * 50 / 1080)
+	
+	if LocalPlayer():GetNWBool("customroundvote") then
+		voteyes:SetValue(1)
+		voteno:SetValue(0)
+	else
+		voteyes:SetValue(0)
+		voteno:SetValue(1)
+	end
+	
+	function voteno:OnChange()
+		voteyes:SetChecked(not voteno:GetChecked())
+		net.Start("firesttt_votecustomround")
+			net.WriteBool(voteyes:GetChecked())
+			net.WriteBool(died)
+		net.SendToServer()
+	end
+	
+	function voteyes:OnChange()
+		voteno:SetChecked(not voteyes:GetChecked())
+		net.Start("firesttt_votecustomround")
+			net.WriteBool(voteyes:GetChecked())
+			net.WriteBool(died)
+		net.SendToServer()
+	end
+	
+	function frame:OnClose()
+		net.Start("firesttt_votecustomround")
+			net.WriteBool(voteyes:GetChecked())
+			net.WriteBool(died)
+		net.SendToServer()
+	end
+	
+	sheet:AddSheet( "Vote", panel1, "icon16/page.png" )
+
+	local panel2 = vgui.Create( "DPanel", sheet )
+	panel2:SetContentAlignment(8) --centers everything
+	
+	local DermaButton = vgui.Create( "DButton", panel2 ) 
+	DermaButton:SetText( "Open Explanation" )					
+	DermaButton:Center()			
+	DermaButton:SetSize( ScrW() * 372/1920, ScrH() * 75/1080 )					
+	DermaButton.DoClick = function()				
+		gui.OpenURL("https://www.zzzzz.dev/ttt/ttt-custom-round-explanation.html")			
+	end
+	
+	sheet:AddSheet( "Explanation", panel2, "icon16/help.png" )
+
+end
--- autorun/server/sv_endroundfun.lua
+++ autorun/server/sv_endroundfun.lua
@@ -1,13 +1,18 @@
 util.AddNetworkString("firesttt_csaycustomround")
+util.AddNetworkString("firesttt_votecustomround")
 
 EndRoundGame = {}
 EndRoundGame.IsActive = false
 EndRoundGame.ActiveGamemode = nil
+EndRoundGame.CurrentModifier = nil
 EndRoundGame.NextRoundCustom = false
 EndRoundGame.NextGamemode = nil
 
 EndRoundGame.Gamemodes = {}
---Load gamemodes
+EndRoundGame.Modifiers = {}
+
+print("[CUSTOM ROUNDS] Loading files....")
+--Load gamemodes and modifiers
 local files, directories = file.Find( "addons/endroundfun/lua/autorun/server/games" .. "/*", "GAME" )
 local counter = 0
 for _,v in pairs(files) do
@@ -15,50 +20,163 @@ for _,v in pairs(files) do
 	counter = counter + 1
 	print("[CUSTOM ROUNDS] Loaded "..counter.." end round gamemodes...."..v)
 end
+--Load gamemodes and modifiers
+local files, directories = file.Find( "addons/endroundfun/lua/autorun/server/modifiers" .. "/*", "GAME" )
+local counter = 0
+for _,v in pairs(files) do
+	include("modifiers/"..v)
+	counter = counter + 1
+	print("[CUSTOM ROUNDS] Loaded "..counter.." end round modifiers...."..v)
+end
+print("[CUSTOM ROUNDS] Loaded files!")
+
+local recentlyvoted = {}
+
+-- VOTING
+function GetVotingRatio()
+	local votedyes = 0
+	local votedno = 0
+	for _,v in pairs(player.GetAll()) do
+		if v:GetNWBool("customroundvote") then
+			votedyes = votedyes + 1
+		else
+			votedno = votedno + 1
+		end
+	end
+	return votedyes / votedno
+end
+
+
+--Determines if ratio of Yes/No beats the target percentage.
+function ShouldCustomRoundHappen(perc)
+	return (GetVotingRatio() >= perc)
+end
+
+--Wipes votes off everyone
+function ResetVotes()
+	for _,v in pairs(player.GetAll()) do
+		v:SetNWBool("customroundvote", false)
+		v:SetNWBool("shownvote", false)
+	end
+end
+
+net.Receive("firesttt_votecustomround", function(len, ply)
+	local castedvote = net.ReadBool()
+	local died = net.ReadBool()
+	ply:SetNWBool("customroundvote", castedvote)
+	print("[CUSTOM ROUND] Received vote from "..ply:Name().." vote content: "..tostring(castedvote))
+	if not table.contains(recentlyvoted, ply) then
+		table.insert(recentlyvoted, ply)
+		local ratio = GetVotingRatio() * 100
+		if not died then
+			BroadcastMsg(Color(255,0,0), "[CUSTOM ROUNDS] ", Color(255,255,255), "The Current Vote Percentage is ", Color(255,0,0), tostring(ratio), "% ", Color(255,255,255), "'Yes'. Vote now with ", Color(255,0,0), "!customvote", Color(255,255,255), ".")
+		end
+		timer.Simple(120, function()
+			table.remove(recentlyvoted, table.find(recentlyvoted, ply))
+		end)
+	end
+end)
+
+hook.Add("PlayerDeath", "handlevoting", function(v, inf, at)
+	local rounds_left = GetGlobalInt("ttt_rounds_left")
+	local shouldshow = (math.random(rounds_left-1) == 1) --Increases chance as game gets closer.
+	if shouldshow and not v:GetNWBool("shownvote") then
+		v:SendLua("ShowVotingFrame(true)")
+		v:SetNWBool("shownvote", true)
+	end
+end)
 
 --ROUND MANAGEMENT
-
+math.randomseed(os.time())
 hook.Add("TTTPrepareRound", "firespossiblyenable", function()
+	EndRoundGame.CurrentModifier = nil--debug purposes
 	SetGlobalBool("EndRoundGameIsActive", EndRoundGame.IsActive)
-	
 	local rounds_left = GetGlobalInt("ttt_rounds_left")
 	
-	if rounds_left ~= 1 and not EndRoundGame.NextRoundCustom then return end
+	--Check if its last round, if we have enough votes or if it was admin forced.
+	if rounds_left ~= 1 and not ShouldCustomRoundHappen(0.5) and not EndRoundGame.NextRoundCustom then return end
 	
+	--If admin forced, reset variable
 	if EndRoundGame.NextRoundCustom then
 		EndRoundGame.NextRoundCustom = false
 	end
 	
-	--Randomly decide to do an activity, currently will just run one every last map
+	local haveModifier = (math.random(2) == 1)
+	if haveModifier then
+		EndRoundGame.CurrentModifier = math.randomchoice(EndRoundGame.Modifiers)
+	end
 	
 	--Enable end round game
 	SetGlobalBool("EndRoundGameIsActive", true)
+	
 	
 	--Decide which game to use if one is not already selected
 	if EndRoundGame.NextGamemode ~= nil then
 		EndRoundGame.ActiveGamemode = EndRoundGame.NextGamemode
 		EndRoundGame.NextGamemode = nil
 	else
-		EndRoundGame.ActiveGamemode = math.randomchoice(EndRoundGame.Gamemodes)
+		local goodtogo = false
+		while goodtogo == false do
+			EndRoundGame.ActiveGamemode = math.randomchoice(EndRoundGame.Gamemodes)
+			if EndRoundGame.CurrentModifier != nil then
+				if EndRoundGame.CurrentModifier.gamemodeBlacklist ~= nil then
+					if table.contains(EndRoundGame.CurrentModifier.gamemodeBlacklist, EndRoundGame.ActiveGamemode.id) then
+						print("[CUSTOM ROUNDS] picked gamemode on blacklist")
+					else
+						goodtogo = true
+						break
+					end
+				else
+					goodtogo = true
+					break
+				end
+			else
+				goodtogo = true
+				break
+			end
+		end
 	end
+	
 	local gm = EndRoundGame.ActiveGamemode
 	net.Start("firesttt_csaycustomround")
 		net.WriteString(gm.name)
 		net.WriteString(gm.description)
+		if EndRoundGame.CurrentModifier then
+			net.WriteString(EndRoundGame.CurrentModifier.name)
+		else
+			net.WriteString("none_included")
+		end
 	net.Broadcast()
+	if EndRoundGame.CurrentModifier ~= nil then
+		EndRoundGame.CurrentModifier.roundPreparing()
+	end
 	gm.roundPreparing()
 end)
 
 hook.Add("TTTBeginRound", "firesbeginEndRoundGame", function(result)
 	if GetGlobalBool("EndRoundGameIsActive") and EndRoundGame.ActiveGamemode ~= nil then
 		EndRoundGame.ActiveGamemode.roundStart()
+		if EndRoundGame.CurrentModifier ~= nil then
+			EndRoundGame.CurrentModifier.roundStart()
+		end
+		--Hook Karma blocker so no one will get penalized if they kill a friendly
+		hook.Add("TTTKarmaGivePenalty", "EndRoundGamefunkarma", function(ply, penalty, victim)
+			return true
+		end)
 	end
 end)
 
 hook.Add("TTTEndRound", "firesdisableEndRoundGame", function(result)
 	if GetGlobalBool("EndRoundGameIsActive") and EndRoundGame.ActiveGamemode ~= nil then
 		EndRoundGame.ActiveGamemode.roundEnd()
+		if EndRoundGame.CurrentModifier ~= nil then
+			EndRoundGame.CurrentModifier.roundEnd()
+		end
 		SetGlobalBool("EndRoundGameIsActive", false)
 		EndRoundGame.ActiveGamemode = nil
+		EndRoundGame.CurrentModifier = nil
+		ResetVotes()
+		--Remove hook so regular rounds work again with Karma.
+		hook.Remove("TTTKarmaGivePenalty", "EndRoundGamefunkarma")
 	end
 end)
